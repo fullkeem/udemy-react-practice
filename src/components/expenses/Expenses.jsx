6c01f7b90685643d487c2b43205a6582c2d88fc0
choongmankim <cm730712@gmail.com>
2022-11-24 00:23:28 +0900

    Add: 년도별 조건부 렌더링 기능 구현

--- src/components/expenses/Expenses.jsx
+++ src/components/expenses/Expenses.jsx
@@ -1,31 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import styled from "styled-components";
+import ExpenseFilter from "./ExpenseFilter";
 
-const Expenses = (props) => {
+const Expenses = ({ items }) => {
+  const [filteredYear, setFilteredYear] = useState("2022");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filterExpense = items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No Expenses Found.</p>;
+
+  if (filterExpense.length > 0) {
+    expensesContent = filterExpense.map((el) => (
+      <ExpenseItem
+        key={el.id}
+        title={el.title}
+        amount={el.amount}
+        date={el.date}
+      />
+    ));
+  }
+
   return (
     <StyledDiv>
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
+      <ExpenseFilter
+        selectedYear={filteredYear}
+        onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {expensesContent}
     </StyledDiv>
   );
 };
