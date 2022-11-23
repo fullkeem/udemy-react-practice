0fcc4c768b54ec8da7a825bb5214c85bfe76a0c4
choongmankim <cm730712@gmail.com>
2022-11-24 02:33:34 +0900

    Add: chart 컴포넌트 및 폴더 생성

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import ChartBar from "./ChartBar";
+import styled from "styled-components";
+
+const Chart = (props) => {
+  const dataPointValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...dataPointValues);
+
+  return (
+    <StyledChart>
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />
+      ))}
+    </StyledChart>
+  );
+};
+
+const StyledChart = styled.div`
+  padding: 1rem;
+  border-radius: 12px;
+  background-color: #f8dfff;
+  text-align: center;
+  display: flex;
+  justify-content: space-around;
+  height: 10rem;
+`;
+
+export default Chart;
