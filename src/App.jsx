eaa7f5b6138fcd4198d945a00b392f0d7918f97b
choongmankim <cm730712@gmail.com>
2022-11-24 00:21:37 +0900

    Fix: 기존 App 컴포넌트 내에 있는 data 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./components/expenses/Expenses";
+import NewExpenses from "./components/newExpenses/NewExpenses";
+
+const DUMMY_EXPENES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevData) => {
+      return [expense, ...prevData];
+    });
+  };
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpenses onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
